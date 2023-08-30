ca1e9bd41148a3377fad68831afe4d9b39738b76
DarioM <manghina.dario@gmail.com>
2023-08-30 15:14:03 +0200

    tabs-pre

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TabViewModule } from 'primeng/tabview';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
-
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     ButtonModule,
+    TabViewModule,
+    TableModule,
+    TabMenuModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,67 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+
+const left = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
+      optional: true,
+    }),
+  ]),
+];
+
+const right = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
+      optional: true,
+    }),
+  ]),
+];
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   animations: [
-    trigger('slideInLeft', [
-      transition(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('1500ms ease-out', style({ transform: 'translateX(0)' })),
-      ]),
-      transition(":leave", [
-        animate('1500ms ease-out', style({ transform: 'translateX(-100%)' })),
-      ])
-    ])
-  ]
+    trigger('animSlider', [
+      transition(':increment', right),
+      transition(':decrement', left),
+    ]),
+  ],
 })
 export class HomePage {
 
   activeItem: number = 0;
-
+  counter: number = 0;
+  products = [
+    {}, {}, {}
+  ]
   constructor() {
 
   }
 
   toggleTab(i: number) {
+    if (i > this.activeItem) {
+      this.onNext();
+    }
+    else {
+      this.onPrevious();
+    }
     this.activeItem = i;
   }
+  onNext() {
+    this.counter++;
+  }
 
+  onPrevious() {
+    this.counter--;
+  }
 }
